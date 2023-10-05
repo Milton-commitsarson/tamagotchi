32eace488cd31446f15278913b78391fe8a08b94
Milton-commitarson <milton.molanderryberg@elev.ga.ntig.se>
2023-10-05 11:28:18 +0200

    Initial tamagotchi puch

---
+++ src/Tamagotchi.java
@@ -0,0 +1,35 @@
+public class Tamagotchi {
+    //Attributes
+    private int hunger;
+    private int boredom;
+    private List<string> words;
+    private bool isAlive;
+    Random.generator = newRandom;
+    public String name;
+
+    //Constructers
+
+
+    //Methods
+    public void Feed(){
+
+    }
+    public void Hi(){
+
+    }
+    public void Teach(String words){
+
+    }
+    public void Tick(){
+
+    }
+    public void PrintStats(){
+
+    }
+    private void ReduceBoredom(){
+
+    }
+
+    // Setters & getters
+
+}
