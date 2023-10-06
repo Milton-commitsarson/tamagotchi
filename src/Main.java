a29667a1ac5d878d9cec61bbfdfdfd07337b68c0
Milton-commitarson <milton.molanderryberg@elev.ga.ntig.se>
2023-10-06 13:43:45 +0200

    tamagotchi second puch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Tamagotchi  mytamagotchi = new Tamagotchi();
 
+        System.out.println("Hi, here is a tamagotchi");
+
+
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("do you want to teach your tamagotchi a new word");
+
+        String svar = myScanner.nextLine();
+
+        if (svar.equals("y")){
+            mytamagotchi.Teach();
+
+            //System.out.println("new value " +myDice.sidesShowing);
+
+            System.out.println("do you want to roll again? Y/N" );
+        }
     }
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tamagotchi {
     //Attributes
     private int hunger;
     private int boredom;
-    private List<string> words;
-    private bool isAlive;
-    Random.generator = newRandom;
+    private ArrayList<String> words = new ArrayList<String>();
+    private boolean isAlive;
+    private Random generator = new Random();
+    Scanner wordScanner = new Scanner(System.in);
     public String name;
 
     //Constructers
@@ -18,6 +24,9 @@ public class Tamagotchi {
 
     }
     public void Teach(String words){
+        System.out.println("Write a word you want your tamagotchi to lern");
+        this.words.add(wordScanner.next());
+        System.out.println(words);
 
     }
     public void Tick(){
